d3282d7d7b1ec50d81b561e760a6b45dce9cef26
dgiacco <dgiaccobasso@gmail.com>
2021-06-06 20:56:02 -0300

    create pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import PostItList from "./Components/PostItList";
-import { v4 as uuidv4 } from "uuid";
-import TrashBin from "./Components/TrashBin";
-import { FaTrashRestore } from "react-icons/fa";
-import useLocalStorage from "./customHooks/useLocalStorage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TrashBin from "./pages/TrashBin";
+import Workspace from "./pages/Workspace";
 
 const App = () => {
-  const [postIts, setPostIts] = useLocalStorage("postits", []);
-
-  const [deletedPostIts, setDeletedPostIts] = useLocalStorage("deletedPostits", []);
-
-  const addPostIt = (text) => {
-    const newPostIt = {
-      id: uuidv4(),
-      text: text,
-    };
-    const newPostIts = [...postIts, newPostIt];
-    setPostIts(newPostIts);
-  };
-
-  const deletePostIt = (id) => {
-    const newPostIts = postIts.filter(postIt => postIt.id !== id);
-    const deletedPostit = postIts.filter(postIt => postIt.id === id);
-    deletedPostIts.push(deletedPostit);
-    setPostIts(newPostIts);
-    setDeletedPostIts(deletedPostIts);
-  };
+  
 
   return (
     <Router>
@@ -35,23 +13,7 @@ const App = () => {
           <TrashBin />
         </Route>
         <Route path="/">
-          <div>
-            <div className="header">
-              <h1>My Post Its</h1>
-            </div>
-            <div className="postit-container">
-              <PostItList
-                postIt={postIts}
-                addPostIt={addPostIt}
-                deletePostIt={deletePostIt}
-              />
-            </div>
-            <div>
-              <Link to="/trashbin">
-                <FaTrashRestore />
-              </Link>
-            </div>
-          </div>
+          <Workspace />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/Workspace.jsx
@@ -0,0 +1,51 @@
+import {  Link } from "react-router-dom";
+import { FaTrashRestore } from "react-icons/fa";
+import PostItList from "../Components/PostItList";
+import { v4 as uuidv4 } from "uuid";
+import useLocalStorage from "../customHooks/useLocalStorage";
+
+
+const Workspace = () => {
+  const [postIts, setPostIts] = useLocalStorage("postits", []);
+
+  const [deletedPostIts, setDeletedPostIts] = useLocalStorage("deletedPostits", []);
+
+  const addPostIt = (text) => {
+    const newPostIt = {
+      id: uuidv4(),
+      text: text,
+    };
+    const newPostIts = [...postIts, newPostIt];
+    setPostIts(newPostIts);
+  };
+
+  const deletePostIt = (id) => {
+    const newPostIts = postIts.filter(postIt => postIt.id !== id);
+    const deletedPostit = postIts.filter(postIt => postIt.id === id);
+    deletedPostIts.push(deletedPostit);
+    setPostIts(newPostIts);
+    setDeletedPostIts(deletedPostIts);
+  };
+
+  return(
+    <div>
+      <div className="header">
+        <h1>My Post Its</h1>
+      </div>
+      <div className="postit-container">
+        <PostItList
+          postIt={postIts}
+          addPostIt={addPostIt}
+          deletePostIt={deletePostIt}
+        />
+      </div>
+      <div>
+        <Link to="/trashbin">
+          <FaTrashRestore />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Workspace;
