5c2cee0977c2581c71a3ebb94ffd697491b83b28
dgiacco <dgiaccobasso@gmail.com>
2021-06-11 12:01:42 -0300

    change post it color

--- src/Components/PostIt.jsx
+++ src/Components/PostIt.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 import { FaRecycle } from "react-icons/fa";
+import classnames from "classnames";
 
 const PostIt = ({ id, text, deletePostIt, isDeleted, restorePostIt, editPostIt }) => {
+  const [isYellow, setisYellow] = useState(true)
+
+  const changeToGreen = () => {
+    setisYellow(false)
+  }
+
+  const changeToYellow = () => {
+    setisYellow(true)
+  }
 
   return (
-    <div className="postit">
+    <div className={classnames({"postit": isYellow, "greenPostIt": !isYellow})}>
       <div className="postit-header">
-        <FaTrash className="deleteButton" onClick={ () => deletePostIt(id) }/>
-        { isDeleted && <FaRecycle className="restoreButton" onClick={ () => restorePostIt(id) } /> }
+        <div>
+          { !isDeleted && <button className="turnYellowButton" onClick={changeToYellow}></button> }
+          { !isDeleted && <button className="turnGreenButton" onClick={changeToGreen}></button> }
+        </div>
+        <div className="deleteAndRestoreButtonContainer" >
+          { isDeleted && <FaRecycle className="restoreButton" onClick={ () => restorePostIt(id) } /> }
+          <FaTrash className="deleteButton" onClick={ () => deletePostIt(id) }/>
+        </div>
       </div>
       <input className="editInput" value={text} onChange={event => editPostIt(event, id)} name="text"></input>
     </div>
